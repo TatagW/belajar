9cbd45d61076900b5672c86c3a09359dffe67fd8
TatagW <tatagwibowo95@gmail.com>
2020-10-28 12:19:21 +0700

    fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function test () {
-    console.log('hahaha')
+    console.log('hahahca')
     console.log('coba edit')
     console.log('akjsdhakjsdha')
     console.log("Zzzzzzzzzzz")
     console.log('kajsdakjsdh')
+    console.log('coba edit')
+    
 
 }
 
--- index1.js
+++ index1.js
@@ -2,4 +2,6 @@ function test () {
     console.log('coba index1')
     console.log('kajsdakjsdh')
     console.log('coba index kedua')
+    console.log('coba edit')
+
 }
